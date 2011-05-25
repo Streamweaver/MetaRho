e1f4da54159dcad8a995f176ade059f5203dde8a
Scott Turnbull <streamweaver@gmail.com>
2011-05-24 23:39:54 -0400

    Added template tag to pull site domain.

---
+++ metarho/sitemeta/templatetags/site_tags.py
@@ -0,0 +1,30 @@
+# file site_tags.py
+#
+# Copyright 2011 Scott Turnbull
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Contains various tags to use related to the blog models and features.
+from django import template
+
+from django.contrib.sites.models import Site
+
+register = template.Library()
+
+@register.simple_tag
+def sitedomain():
+    '''Returns the URL of the default site.'''
+    try:
+        return Site.objects.get_current().domain
+    except Site.DoesNotExist:
+        return None
