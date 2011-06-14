14a57e48868e19c20db1ee258e2279ba7f105f67
Scott Turnbull <streamweaver@gmail.com>
2011-06-14 00:38:20 -0400

    Created Gravitar Template Tag in Accounts.

---
+++ metarho/account/templatetags/account_tags.py
@@ -0,0 +1,48 @@
+### gravatar.py ###############
+### place inside a 'templatetags' directory inside the top level of a Django app (not project, must be inside an app)
+### at the top of your page template include this:
+### {% load gravatar %}
+### and to use the url do this:
+### <img src="{% gravatar_url 'someone@somewhere.com' %}">
+### or
+### <img src="{% gravatar_url sometemplatevariable %}">
+### just make sure to update the "default" image path below
+
+from django import template
+import urllib, hashlib
+
+from metarho import settings
+
+
+GRAVATAR_BASEURL = getattr(settings, "GRAVATAR_BASEURL", "http://www.gravatar.com/avatar/")
+GRAVATAR_DEFAULT_IMAGE = getattr(settings, "GRAVATAR_DEFAULT_IMAGE", "")
+GRAVATAR_SIZE = getattr(settings, "GRAVATAR_SIZE", 40)
+
+register = template.Library()
+
+def gravitar_url(email, size):
+
+    attrs = {
+        'd': GRAVATAR_DEFAULT_IMAGE,
+        's': size
+    }
+
+    gravatar_url = "%s%s/?" % (GRAVATAR_BASEURL, hashlib.md5(email.lower()).hexdigest())
+    gravatar_url += urllib.urlencode(attrs)
+
+    return {'gravatar': {'url': gravatar_url, 'size': size}}
+
+@register.inclusion_tag('account/snippets/gravitar.xhtml')
+def gravatar_for_email(email, size=GRAVATAR_SIZE):
+    """
+    Renders a gravatar image for user with the specified email via a template.
+
+    {% gravatar_for_email "user@email.com" 40 %}
+
+    :param email:  String representing the users email.
+    :param size: Size of gravatar to use in pixels.  OPTIONAL
+    
+    """
+    email = "%s" % email
+    size = int(size)
+    return gravitar_url(email, size)
