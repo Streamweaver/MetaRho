ecb31b0d3156ed90c5ecee88e0fefeb002eaaad0
Scott Turnbull <streamweaver@gmail.com>
2011-06-18 23:38:18 -0400

    Added profiling middleware for testing.

--- metarho/middleware.py
+++ metarho/middleware.py
@@ -1,38 +1,49 @@
-# Taken from http://djangosnippets.org/snippets/70/
-# Author: limodou@gmail.com
-# version: 0.2
-# Profile request of django
-#
+# Taken from http://djangosnippets.org/snippets/186
 
+import sys
+import tempfile
 import hotshot
-import os
-import time
+import hotshot.stats
 from django.conf import settings
+from cStringIO import StringIO
 
-PROFILE_DATA_DIR = "./profile"
 class ProfileMiddleware(object):
-    def process_request(self, request):
-        path = getattr(settings, 'PROFILE_DIR', PROFILE_DATA_DIR)
-        if not os.path.exists(path):
-            os.makedirs(path)
-            os.chmod(path, 0755)
-#        profname = "%s.%.3f.prof" % (request.path.strip("/").replace('/', '.'), time.time())
-        profname = "%s.prof" % (request.path.strip("/").replace('/', '.'))
-        profname = os.path.join(PROFILE_DATA_DIR, profname)
-        try:
-            self.prof = prof = hotshot.Profile(profname)
-            prof.start()
-        except:
-            self.prof = None
+    """
+    Displays hotshot profiling for any view.
+    http://yoursite.com/yourview/?prof
 
-#    def process_view(self, request, callback, callback_args, callback_kwargs):
-#        try:
-#            return prof.runcall(callback, request, *callback_args, **callback_kwargs)
-#        finally:
-#            prof.close()
-#
+    Add the "prof" key to query string by appending ?prof (or &prof=)
+    and you'll see the profiling results in your browser.
+    It's set up to only be available in django's debug mode,
+    but you really shouldn't add this middleware to any production configuration.
+    * Only tested on Linux
+    """
+    def process_request(self, request):
+        if settings.DEBUG and request.GET.has_key('prof'):
+            self.tmpfile = tempfile.NamedTemporaryFile()
+            self.prof = hotshot.Profile(self.tmpfile.name)
+
+    def process_view(self, request, callback, callback_args, callback_kwargs):
+        if settings.DEBUG and request.GET.has_key('prof'):
+            return self.prof.runcall(callback, request, *callback_args, **callback_kwargs)
 
     def process_response(self, request, response):
-        if self.prof:
+        if settings.DEBUG and request.GET.has_key('prof'):
             self.prof.close()
+
+            out = StringIO()
+            old_stdout = sys.stdout
+            sys.stdout = out
+
+            stats = hotshot.stats.load(self.tmpfile.name)
+            #stats.strip_dirs()
+            stats.sort_stats('time', 'calls')
+            stats.print_stats()
+
+            sys.stdout = old_stdout
+            stats_str = out.getvalue()
+
+            if response and response.content and stats_str:
+                response.content = "<pre>" + stats_str + "</pre>"
+
         return response
